7741da10ddd9426480aea1d2b68ed340c1398590
KodlandLVL3 <155145450+KodlandLVL3@users.noreply.github.com>
2024-04-22 21:22:22 +0300

    first commit

--- tests/test_calculator_program.py
+++ tests/test_calculator_program.py
@@ -6,9 +6,24 @@ def test_calculate_addition():
 
 def test_calculate_division():
     assert calculate(8, 2, '/') == 4
+    assert calculate(8, 0, '/') == 'Ошибка: Деление на ноль.'
 
 def test_calculate_unknown_operation():
     assert calculate(5, 5, 'unknown') == "Неизвестная операция."
+
+def test_calculate_sub():
+    assert calculate(8, 5, '-') == 3
+    assert calculate(8, 9, '-') == -1
+    assert calculate(-9, 9, '-') == -18
+
+
+def test_calculate_mul():
+    assert calculate(-8, -5, '*') == 40
+    assert calculate(1.5, 3, '*') == 4.5
+
+
+def test_calculate():
+    assert calculate(8, '111', '*') == 888
 
 '''
 Задача. В настоящий момент реализовано три unit-теста
